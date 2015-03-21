a1926dabda4d37b22c212c6b74a412ab7fd9b2d8
Pablo Farfan Chavez <pablochavez15@gmail.com>
2015-03-21 14:01:28 -0300

    gitignore modificado

---
+++ application/Config.php
@@ -0,0 +1,19 @@
+<?php
+define('DEFAULT_CONTROLLER','index');
+define('BASE_URL','http://localhost');
+
+define('APP_NAME','');
+define('SESSION_TIME', 60);
+define('SESSION_TIME_CARRO', 5);
+
+define('DB_HOST','localhost');
+define('DB_USER','root');
+define('DB_PASS','');
+define('DB_NAME','');
+define('DB_CHAR','utf8');
+
+define('EMAIL_USERNAME','');
+define('EMAIL_PASSWORD','');
+define('EMAIL_EMISOR','');
+
+?>
