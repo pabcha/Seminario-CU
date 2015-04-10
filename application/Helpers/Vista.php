6d80f37b46ea48a756d5ca18442a5f40e74329d8
Pablo Farfan Chavez <pablochavez15@gmail.com>
2015-04-10 18:15:34 -0300

    Fix + funcion para paginador de vista search

--- application/Helpers/Vista.php
+++ application/Helpers/Vista.php
@@ -110,6 +110,19 @@ class Vista
 		return BASE_URL.'index/categoria/'.$id.'?page='.$pag.$orderby;
 	}
 
+	public static function hrefPagSearch($pag)
+	{
+		$a = array();
+
+		if (!empty($_GET['q'])) array_push($a, 'q='.$_GET['q']);
+		if (isset($_GET['min'])) array_push($a, 'min='.$_GET['min']);
+		if (isset($_GET['max'])) array_push($a, 'max='.$_GET['max']);
+		if (isset($_GET['marca']) and $_GET['marca'] != 0) array_push($a, 'marca='.$_GET['marca']);
+		array_push($a, 'page='.$pag);
+
+		return BASE_URL.'search?'.implode('&', $a);;
+	}
+
 	public static function is_selected($value, $get)
 	{
 		return ($value == $get) ? 'selected="selected"' : '';
@@ -133,8 +146,6 @@ class Vista
 
 		if ( isset($_GET[$key]) )
 		{
-			/*if()
-			d('h');*/
 			$get = $_GET[$key];
 		}
 
